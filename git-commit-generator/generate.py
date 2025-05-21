11b0d70c72708f686e6f0e3b4dbb132e64a22799
LeopardCheetah <hawkswithmath@gmail.com>
2025-05-21 08:49:48 -0700

    [generated] tunning

--- git-commit-generator/generate.py
+++ git-commit-generator/generate.py
@@ -1,2 +1,6 @@
-test
-commit 2
+# see https://github.com/ngerakines/commitment/tree/main for the cool main repo
+# literally just forked the thing
+
+# command i use:
+# git config --global alias.yolo '!git commit -m "[generated] $(curl -s https://whatthecommit.com/index.txt)"' 
+# use with 'git yolo'
